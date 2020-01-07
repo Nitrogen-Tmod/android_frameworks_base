3824a21e7c1a506f1f8e9bc1826fedd23f896f5f
00day0 <therandomuser11@gmail.com>
2020-01-07 20:15:27 +0100

    base: Blur behind quick settings [1/2]
    Thanks to @Dil3mm4 utility which allows to take screenshots and blur them
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: iKeramat <ikeramat80@gmail.com>
    
    Change-Id: I353071cce27f00d1cd9831b6dd9d50d649aa29fe
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2249,6 +2249,7 @@ public class NotificationPanelView extends PanelView implements
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScroller.setAlpha(alpha);
+        mStatusBar.updateBlurVisibility();
     }
 
     private float getFadeoutAlpha() {
